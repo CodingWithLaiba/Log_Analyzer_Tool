4f7947e9992852b9f71e118001cd5237fef7f3e3
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-22 15:35:40 +0500

    Refactor App component to implement file upload and log analysis using axios; add sample log file for testing

--- log-tool/src/App.jsx
+++ log-tool/src/App.jsx
@@ -1,122 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
+import { useState } from "react";
+import axios from "axios";
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState(null);
+  const [result, setResult] = useState(null);
+  const uploadFile = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const res = await axios.post("http://localhost:5000/analyze", formData);
+
+      setResult(res.data);
+    } catch (error) {
+      console.log(error);
+      alert("Something went wrong while analyzing file");
+    }
+  };
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+      <h2>Log Analyzer tool</h2>
+      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+      <button onClick={uploadFile}>Upload file to analyze</button>
+      {result && (
         <div>
-          <h1>Get started</h1>
+          <h4>Analyze Result:</h4>
+          <p>Total Lines: {result.total_lines}</p>
+          <p>Bad Lines: {result.bad_lines}</p>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            <b>Endpoints:</b>
           </p>
+          <pre>{JSON.stringify(result.endpoints, null, 2)}</pre>
+          <p>
+            <b>Status Counts:</b>
+          </p>
+          <pre>{JSON.stringify(result.status_counts, null, 2)}</pre>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
